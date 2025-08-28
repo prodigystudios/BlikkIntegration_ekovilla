7cfe3818018f6eb7c332421ecacc6bfc3ff0bbc9
william ali <william.ali@hotmail.se>
2025-08-28 20:47:09 +0200

    fix: Update header height and content gap for improved PWA layout consistency for androind and iOS

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,10 +19,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <link rel="icon" href="/favicon.ico" sizes="any" />
       <link rel="apple-touch-icon" href="/apple-touch-icon.png" sizes='180x180'/>
       <link rel="manifest" href="/manifest.webmanifest" />
-      <meta name="theme-color" content="#0ea5e9" />
+  <meta name="theme-color" content="#ffffff" />
       <meta name="mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
-      <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+  <meta name="apple-mobile-web-app-status-bar-style" content="default" />
       <meta name="apple-mobile-web-app-title" content="Egenkontroll" />
   <meta name="color-scheme" content="light" />
     </head>
